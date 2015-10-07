9861d53d4145a4acb226bcbadaabdd57faf3b791
Ryan Grippeling <R.Grippeling@hotmail.com>
2015-10-07 09:55:12 +0200

    Removal of private API key
    You might want to consider removing this private key from the repository and making a config file instead.

--- classes/functions.php
+++ classes/functions.php
@@ -2,7 +2,7 @@
 	
 	function getData($url) {
 		
-		$context = stream_context_create(array('http' => array('header' => 'X-API-Key: 57559c5a83f04bb08e874e783022caa3')));
+		$context = stream_context_create(array('http' => array('header' => 'X-API-Key: ')));
 		
 		$res = file_get_contents($url, false, $context);
 		return $res;
